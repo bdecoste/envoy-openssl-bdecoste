222dbb759f4373189290bb1560b574f3ad546299
William DeCoste <bdecoste@gmail.com>
2019-02-11 14:03:13 -0800

    update

--- openssl.sh
+++ openssl.sh
@@ -5,8 +5,10 @@ TARGET=$2
 
 pushd ${SOURCE_DIR}
   git fetch upstream
-  git checkout master
-  git reset --hard upstream/master
+#  git checkout master
+#  git reset --hard upstream/master
+  git checkout envoy_for_proxy
+  git reset --hard b3be5713f2100ab5c40316e73ce34581245bd26a
 popd
 
 if [ "$TARGET" == "RESET" ]; then
